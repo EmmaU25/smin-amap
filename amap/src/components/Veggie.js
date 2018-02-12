05fcffb1d2be2bda73730ea36df3ba9757275784
Ernesto emmanuel Yah Lopez <ernestoemmanuel.lopez@hotmail.com>
2018-02-12 11:22:10 +0100

    Verifier la disponibilite d'un legume

--- amap/src/components/Veggie.js
+++ amap/src/components/Veggie.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 class Veggie extends React.Component{
 
+
+	veggie_en_stock(){
+		if(this.props.details.status == "available"){
+			return <button onClick={(e)=>this.props.add_order(this.props.index,this.props.details)}>"Ajouter au panier"</button>
+		}else{
+			return <button disabled='true'>"pas plus en stock"</button>
+		}
+	}
+
 	render(){
 		const {details} = this.props;
 
@@ -12,7 +21,7 @@ class Veggie extends React.Component{
 					<span className="price"> ${details.price*10/1000 } </span>
 				</h3>
 				<p> {details.desc} </p>
-				<button>Ajouter au panier</button>
+				{this.veggie_en_stock()}
 			</li>
 		)
 	}
